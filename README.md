b02d51fb1404fa998e5496b7f2b79f6de138dd2d
Shengyi Li <shengyi.li@sunwing.ca>
2025-02-27 15:13:04 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Olark chatbox language switch and customize text and message.
+
+## Environment
+
+- Node: 22.14.0
+
+`npm i && npm start`
+
+## Expected Result:
+
+---
+
+On our website, we want the Olark chatbox to switch languages based on the website's page language (English or French) and ensure a seamless language transition. Additionally, we need the chatbox title, including the agent's online and offline status, as well as messages, to update accordingly.
+
+## Reproduction:
+
+https://olark-lang-switch-testing.netlify.app/
+
+You can pass the olarkAppId query parameter to allow Olark to identify the default ID.
+
+Example: https://olark-lang-switch-testing.netlify.app?olarkAppId=12456
+
+**Local (`npm start`)**
+**Netlify**
+
+**The customized messages have the Override as prefix**
+
+# Olark Chatbox Localization
+
+This document outlines the behavior of Olark chatbox localization when switching between English and French.
+
+## Localization Behavior
+
+| Parameter                  | Initial Page (English) | Initial Page (French) | Switch from English to French | Switch from French to English |
+| -------------------------- | ---------------------- | --------------------- | ----------------------------- | ----------------------------- |
+| `locale.unavailable_title` | ✅ English             | ✅ French             | ❌ English                    | ❌ French                     |
+| `locale.away_message`      | ✅ English             | ✅ French             | ✅ French                     | ❌ French                     |
+| `locale.welcome_title`     | ❌ Default Title       | ❌ Default Title      | ❌ English                    | ❌ French                     |
+| `locale.name_input_text`   | ✅ English             | ✅ French             | ❌ English                    | ❌ French                     |
+| `locale.email_input_text`  | ✅ English             | ✅ French             | ❌ English                    | ❌ French                     |
+| `locale.phone_input_text`  | ✅ English             | ✅ French             | ❌ English                    | ❌ French                     |
+
+## Instructions
+
+- To trigger the Olark chatbox, click the Expand Olark Chatbox button
+
+- To switch languages, click the respective button (`Switch to French` or `Switch to English`).
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+
+const App = () => {
+  // Load language from localStorage or default to English
+  const [language, setLanguage] = useState(
+    localStorage.getItem("appLanguage") || "English"
+  );
+
+  const toggleLanguage = () => {
+    const newLanguage = language === "English" ? "French" : "English";
+    setLanguage(newLanguage);
+    localStorage.setItem("appLanguage", newLanguage); // Save to localStorage
+  };
+
+  const ExpandOlarkChatbox = () => {
+    window.olark("api.box.expand");
+  };
+
+  const getOlarkAppIdFromQuery = () => {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("olarkAppId") || process.env.REACT_APP_OLARK_SITE_ID;
+  };
+
+  const olarkAppId = getOlarkAppIdFromQuery();
+
+  useEffect(() => {
+    if (window.olark && typeof window.olark === "function") {
+      window.olark.identify(olarkAppId);
+      console.warn("Language Change");
+
+      if (language === "French") {
+        window.olark("api.box.setLocale", "fr-FR");
+        window.olark.configure("system.localization", "fr-FR");
+        window.olark.configure(
+          "locale.unavailable_title",
+          "Besoin d'aide? - Override"
+        );
+        window.olark.configure(
+          "locale.away_message",
+          "(Override)Nous ne sommes pas disponibles pour l&rsquo;instant!"
+        );
+        window.olark.configure(
+          "locale.welcome_title",
+          "Override Clavardez avec nous"
+        );
+        window.olark.configure("locale.name_input_text", "Override Nom");
+        window.olark.configure("locale.email_input_text", "Override Courriel");
+        window.olark.configure("locale.phone_input_text", "Override Téléphone");
+      } else {
+        window.olark("api.box.setLocale", "en-US");
+        window.olark.configure("system.localization", "en-US");
+        window.olark.configure(
+          "locale.unavailable_title",
+          "Need help? - Override"
+        );
+        window.olark.configure(
+          "locale.away_message",
+          "(Override)Sorry we're not available to chat right now! "
+        );
+
+        window.olark.configure("locale.welcome_title", "Override Welcome");
+        window.olark.configure("locale.name_input_text", "Override Name Input");
+        window.olark.configure(
+          "locale.email_input_text",
+          "Override Email Input"
+        );
+        window.olark.configure(
+          "locale.phone_input_text",
+          "Override Phone Input"
+        );
+      }
+    }
+  }, [language, olarkAppId]);
+
+  return (
+    <div className="working-block">
+      <h1>Current Language: {language}</h1>
+      <button onClick={toggleLanguage}>
+        Switch to {language === "English" ? "French" : "English"}
+      </button>
+
+      <button onClick={ExpandOlarkChatbox}>Expand Olark Chatbox</button>
+    </div>
+  );
+};
+
+export default App;
